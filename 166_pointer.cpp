128f4134de2e4801983fb4dbc82594925b549b6e
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-05-08 14:56:40 +0700

    object mhs

--- 166_pointer.cpp
+++ 166_pointer.cpp
@@ -5,4 +5,15 @@ class mahasiswa {
 public:
 	int nim;
 	void ShowNim();		//deklarasi method
+
 };
+
+void mahasiswa::ShowNim() {		//implementasi method di luar class
+	cout << "No Induk = " << nim << endl;
+
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 };		//object mhs
+}
